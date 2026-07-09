cd8883553700274a9a649210b3cc2eba37767ff2
0xDeon <oluwadamilare_daniel@outlook.com>
2026-07-09 17:20:44 +0100

    feat: compose landing page sections

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -1,14 +1,9 @@
 import { Hero } from '../components/sections/hero'
-import { ProtocolViz } from '../components/sections/protocol-viz'
-import { Marketplace } from '../components/sections/marketplace'
-import { Agents } from '../components/sections/agents'
-import { Reputation } from '../components/sections/reputation'
-import { Architecture } from '../components/sections/architecture'
-import { Security } from '../components/sections/security'
-import { LeaderboardPreview } from '../components/sections/leaderboard-preview'
+import { HowItWorks } from '../components/sections/how-it-works'
 import { Ecosystem } from '../components/sections/ecosystem'
-import { Roadmap } from '../components/sections/roadmap'
-import { FAQ } from '../components/sections/faq'
+import { ForBuilders } from '../components/sections/for-builders'
+import { FinalCta } from '../components/sections/final-cta'
+import { Faq } from '../components/sections/faq'
 import { Navbar } from '../components/layout/navbar'
 import { Footer } from '../components/layout/footer'
 
@@ -16,18 +11,13 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main>
+      <main className="pt-16">
         <Hero />
-        <ProtocolViz />
-        <Marketplace />
-        <Agents />
-        <Reputation />
-        <Architecture />
-        <Security />
-        <LeaderboardPreview />
+        <HowItWorks />
         <Ecosystem />
-        <Roadmap />
-        <FAQ />
+        <ForBuilders />
+        <Faq />
+        {/* <FinalCta /> */}
       </main>
       <Footer />
     </>
